f5266b64f2ce4030a8beb0a52abca2ef7cae76e3
wuyue <wuyue92tree@163.com>
2017-02-06 15:57:55 +0800

    proxy_manager添加static目录

--- proxy_manager/proxy_manager/settings/base.py
+++ proxy_manager/proxy_manager/settings/base.py
@@ -17,7 +17,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SUIT_CONFIG = {
     'ADMIN_NAME': u'代理后台管理',
